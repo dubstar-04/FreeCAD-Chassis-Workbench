ad62a9c710afb5440c98d5c98a2222baf4b2fdf1
Dan <s.d.wood.82@googlemail.com>
2021-02-04 19:25:04 +0000

    backup

--- ChassisScripts/frontSuspension.py
+++ ChassisScripts/frontSuspension.py
@@ -1,10 +1,12 @@
 import os
+import math
 
 from PySide import QtCore, QtGui
 
 import FreeCAD
 import FreeCADGui
 import Part
+import DraftVecUtils
 
 __dir__ = os.path.dirname(__file__)
 iconPath = os.path.join(os.path.dirname(__dir__), 'Gui' + os.sep + 'Icons')
@@ -54,7 +56,17 @@ class frontSuspension:
     def execute(self, fp):
         "'''Do something when doing a recomputation, this method is mandatory'''"
         FreeCAD.Console.PrintMessage("Recompute Front suspension feature\n")
-        self.draw(fp)
+
+        hardpoints = {
+            "LFHP": fp.LFHP,
+            "LRHP": fp.LRHP,
+            "UFHP": fp.UFHP,
+            "URHP": fp.URHP,
+            "UrLHP": fp.UrLHP,
+            "UrUHP": fp.UrUHP
+        }
+
+        self.draw(fp, hardpoints)
 
     def __getstate__(self):
         return None
@@ -62,7 +74,7 @@ class frontSuspension:
     def __setstate__(self, state):
         return None
 
-    def draw(self, fp):
+    def draw(self, fp, hp):
 
         geom = []
 
@@ -72,32 +84,97 @@ class frontSuspension:
 
         # upper wishbones
         # right
-        geom.append(Part.Edge(Part.LineSegment(fp.UFHP, fp.UrUHP)))
-        geom.append(Part.Edge(Part.LineSegment(fp.URHP, fp.UrUHP)))
+        geom.append(Part.Edge(Part.LineSegment(hp["UFHP"], hp["UrUHP"])))
+        geom.append(Part.Edge(Part.LineSegment(hp["URHP"], hp["UrUHP"])))
         # left
-        LeftUFHP = FreeCAD.Vector(-fp.UFHP.x, fp.UFHP.y, fp.UFHP.z)
-        LeftURHP = FreeCAD.Vector(-fp.URHP.x, fp.URHP.y, fp.URHP.z)
-        LeftUrUHP = FreeCAD.Vector(-fp.UrUHP.x, fp.UrUHP.y, fp.UrUHP.z)
+        LeftUFHP = FreeCAD.Vector(-hp["UFHP"].x, hp["UFHP"].y, hp["UFHP"].z)
+        LeftURHP = FreeCAD.Vector(-hp["URHP"].x, hp["URHP"].y, hp["URHP"].z)
+        LeftUrUHP = FreeCAD.Vector(-hp["UrUHP"].x, hp["UrUHP"].y, hp["UrUHP"].z)
         geom.append(Part.Edge(Part.LineSegment(LeftUFHP, LeftUrUHP)))
         geom.append(Part.Edge(Part.LineSegment(LeftURHP, LeftUrUHP)))
 
         # lower wishbone
         # right
-        geom.append(Part.Edge(Part.LineSegment(fp.LFHP, fp.UrLHP)))
-        geom.append(Part.Edge(Part.LineSegment(fp.LRHP, fp.UrLHP)))
+        geom.append(Part.Edge(Part.LineSegment(hp["LFHP"], hp["UrLHP"])))
+        geom.append(Part.Edge(Part.LineSegment(hp["LRHP"], hp["UrLHP"])))
         # left
-        LeftLFHP = FreeCAD.Vector(-fp.LFHP.x, fp.LFHP.y, fp.LFHP.z)
-        LeftLRHP = FreeCAD.Vector(-fp.LRHP.x, fp.LRHP.y, fp.LRHP.z)
-        LeftUrLHP = FreeCAD.Vector(-fp.UrLHP.x, fp.UrLHP.y, fp.UrLHP.z)
+        LeftLFHP = FreeCAD.Vector(-hp["LFHP"].x, hp["LFHP"].y, hp["LFHP"].z)
+        LeftLRHP = FreeCAD.Vector(-hp["LRHP"].x, hp["LRHP"].y, hp["LRHP"].z)
+        LeftUrLHP = FreeCAD.Vector(-hp["UrLHP"].x, hp["UrLHP"].y, hp["UrLHP"].z)
         geom.append(Part.Edge(Part.LineSegment(LeftLFHP, LeftUrLHP)))
         geom.append(Part.Edge(Part.LineSegment(LeftLRHP, LeftUrLHP)))
 
         # uprights
-        geom.append(Part.Edge(Part.LineSegment(fp.UrLHP, fp.UrUHP)))
+        geom.append(Part.Edge(Part.LineSegment(hp["UrLHP"], hp["UrUHP"])))
         geom.append(Part.Edge(Part.LineSegment(LeftUrLHP, LeftUrUHP)))
+
+        ani = animateSuspension()
+        ani.start(hp)
 
         resPart = Part.Compound(geom)
         fp.Shape = resPart
+
+
+class animateSuspension:
+    def __init__(self):
+
+        self.timer = QtCore.QTimer()
+        self.iterations = 0
+        self.angle = 0
+        self.increment = 0.1
+        self.upperlimit = 5
+        self.lowerLimit = -2
+        self.hardpoints = {}
+
+        QtCore.QObject.connect(self.timer, QtCore.SIGNAL("timeout()"), self.animate)
+
+    def start(self, hp):
+
+        print("starting animation")
+
+        self.hardpoints = hp
+        self.timer.start(10)
+        """
+        try:
+            #self.timer.timeout.connect(self.animate)
+            print("start timer")
+            self.timer.start(10)
+        except:
+            print("animation failed")
+            self.timer.stop()
+        """
+
+    def animate(self):
+
+        self.angle += self.increment
+
+        print("angle:", self.angle)
+
+        print("rotated vector:", self.rotateVector(self.hp["UrUHP"], self.hp["UFHP"], self.hp["URHP"], self.angle))
+
+        if self.angle > self.upperlimit or self.angle < self.lowerLimit:
+            self.increment = 0 - self.increment
+            self.iterations += 1
+
+        if self.iterations > 4:
+            self.timer.stop()
+
+    def rotateVector(self, vec, axisVecStart, axisVecEnd, angle):
+        """
+        rotate vector: vec
+        about axis defined by: axisVecStart and axisVecEnd
+        by angle in degrees: angle
+        """
+
+        if angle == 0:
+            return vec
+
+        u = vec.sub(axisVecStart)
+        angle = math.radians(5)
+        axis = axisVecEnd.sub(axisVecStart)
+        v = DraftVecUtils.rotate(u, angle, axis).add(axisVecStart)
+
+        return v
 
 
 class ViewProviderFrontSuspension:
